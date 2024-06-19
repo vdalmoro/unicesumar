2ee3572e191f89b0b8115f92dbb30843f6c81ef2
Vinicius Dal Moro <vinimegasxlr@gmail.com>
2024-06-19 00:24:22 -0300

    Teste controle de versão.

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Olá, Mundooooo!");
         System.out.println("Teste!");
+
+        //Adicionando alteração no codigo
+        System.out.println("Este é um exemplo de controle de versão.");
     }
 }

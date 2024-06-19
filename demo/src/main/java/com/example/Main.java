5b539bdeecc3854af600a9ba6f648bd82f1cf393
Vinicius Dal Moro <vinimegasxlr@gmail.com>
2024-06-18 23:41:07 -0300

    Adicionado teste.

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -3,5 +3,6 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Olá, Mundooooo!");
+        System.out.println("Teste!");
     }
 }
